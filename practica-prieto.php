4d6931bcce2b22942f93e7c9daacaeff77e9e804
Antonio <pryet2@gmail.com>
2022-05-03 13:21:12 +0200

    commit inicial

---
+++ practica-prieto.php
@@ -0,0 +1,37 @@
+<?php
+/**
+* Suma y resta dos números
+*
+* Sumna y resta de dos números con tipado
+* estricto (float)
+*@author Antonio Prieto
+*@version 1.0
+*/
+
+declare(strict_types=1);
+
+/**
+*@param float num1 primer sumando
+*@param float num2 segundo sumando
+*@return float resultado de la suma
+*@internal no controla el desbordamiento del float
+*/
+function sumar(float $num1,float $num2):float{
+return $num1+$num2;
+}
+
+/**
+*@param float num1 primer número a restar
+*@param float num2 segundo número a restar
+*@return float resultado de la resta
+*/
+function restar(float $num1,float $num2):float{
+return $num1-$num2;
+}
+
+
+
+
+
+
+?>
